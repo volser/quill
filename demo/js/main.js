02b34de439b0baadd5a10310633b5d5bdfbf6817
luowei <luowei@rensideMacBook-Pro.local>
2020-06-11 02:05:02 +0800

    [pick][feature]Add tableDeltaPaser function for old delta of quill native table

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,6 +1,32 @@
 import 'assets/snow.styl'
 import '../styles/style.styl'
 import Quill from 'root/quill'
+import Delta from 'quill-delta'
+
+import { tableDeltaParser } from '../../modules/clickup-table/utils'
+
+const oldTableDelta = new Delta({
+  "ops":[
+    {"insert":"234\n3243\n"},
+    {"insert":"1\n2\n3\n","attributes":{"table":"row-z2be"}},
+    {"attributes":{"table":"row-xdoa"},"insert":"4\n5\n6\n"},
+    {"attributes":{"table":"row-5eyr"},"insert":"1\n2\n3\n"},
+    {"insert":"\n"}
+  ]
+})
+
+const oldTableDelta2 = new Delta({
+  "ops":[
+    {"insert":"234\n3243\n"},
+    {"insert":"234\n234234\n","attributes":{"table":"row-z2be"}},
+    {"insert":{image: "http://img3.imgtn.bdimg.com/it/u=1267761530,2716523653&fm=26&gp=0.jpg"}},
+    {"insert":"234"},
+    {"insert":"\n","attributes":{"table":"row-z2be"}},
+    {"attributes":{"table":"row-xdoa"},"insert":"234\n234234\n234234\n"},
+    {"attributes":{"table":"row-5eyr"},"insert":"23423\n234234\n234234\n"},
+    {"insert":"\n"}
+  ]
+})
 
 window.onload = () => {
   const quill = new Quill('#editor', {
@@ -9,6 +35,9 @@ window.onload = () => {
       table: true
     }
   })
+
+  // test parse old table delta to new
+  // quill.setContents(tableDeltaParser(oldTableDelta))
 
   const tableModule = quill.getModule('table')
 
@@ -20,5 +49,10 @@ window.onload = () => {
   document.querySelector('#get-delta')
     .addEventListener('click', () => {
       console.log(quill.getContents())
+    }, false)
+
+  document.getElementById('focus-cell')
+    .addEventListener('click', () => {
+      console.log(Quill.find(quill.scroll.domNode.querySelector('table')))
     }, false)
 }
--- modules/clickup-table/utils.js
+++ modules/clickup-table/utils.js
@@ -1,3 +1,11 @@
+import Delta from "quill-delta"
+
+function cellId() {
+  const id = Math.random()
+    .toString(36)
+    .slice(2, 8)
+  return `cell-${id}`
+}
 /**
  * _omit
  * @param  {Object} obj         target Object
@@ -10,4 +18,120 @@ export function _omit (obj, uselessKeys) {
       acc :
       Object.assign({}, acc, { [key]: obj[key] })
   }, {})
+}
+
+// compatible with old native table delta in quilljs
+export function tableDeltaParser(oldDelta) {
+  let delta = new Delta()
+  const tokens = scanTableColumnTokens(oldDelta)
+  let columnCount = 0
+  let currentToken = null
+
+  delta = oldDelta.reduce((newDelta, op) => {
+    if (
+      op.insert &&
+      typeof op.insert === 'string'
+    ) {
+      if (op.attributes &&
+        op.attributes.table) {
+        currentToken = tokens[0]
+        while(currentToken) {
+          if (currentToken === op.attributes.table) {
+            tokens.shift()
+            columnCount += 1
+            currentToken = tokens[0]
+          } else {
+            tokens.shift()
+            currentToken = tokens[0]
+          }
+        }
+
+        // insert tableCol
+        if (columnCount > 0) {
+          newDelta.insert(
+            new Array(columnCount).fill(0).map(() => '\n').join(''),
+            { 'table-col': { width: "150" } }
+          )
+          columnCount = 0
+        }
+
+        const lines = []
+        let insertStr = op.insert
+        let start = 0
+        for (let i = 0; i < op.insert.length; i++) {
+          if (insertStr.charAt(i) === '\n') {
+            if (i === 0) {
+              lines.push('\n')
+            } else {
+              lines.push(insertStr.substring(start, i))
+              lines.push('\n')
+            }
+            start = i + 1
+          }
+        }
+
+        const tailStr = insertStr.substring(start)
+        if (tailStr) lines.push(tailStr)
+
+        lines.forEach(str => {  
+          if (str === '\n') {
+            newDelta.insert(str, Object.assign(
+              {},
+              { row: op.attributes.table, rowspan: 1, colspan: 1 },
+              { 'table-cell-line': { row: op.attributes.table, rowspan: 1, colspan: 1, cell: cellId() } },
+              _omit(op.attributes, ['table'])
+            ))
+          } else {
+            newDelta.insert(str, _omit(op.attributes, ['table']))
+          }
+        })
+      } else {
+        const count = op.insert.split('').filter(char => char === '\n').length
+        new Array(count).fill(0).forEach(() => {
+          tokens.shift()
+        })
+        columnCount = 0
+        newDelta.insert(op.insert, op.attributes)  
+      } 
+    } else {
+      newDelta.insert(op.insert, op.attributes)
+    }
+
+    return newDelta
+  }, new Delta())
+
+  return delta
+}
+// help scan the count of columns for each table in old delta
+function scanTableColumnTokens(oldDelta) {
+  return oldDelta.reduce((countMap, op) => {
+    if(
+      op.insert &&
+      typeof op.insert === 'string'
+    ) {
+      const lines = []
+      let insertStr = op.insert
+      let start = 0
+      for (let i = 0; i < op.insert.length; i++) {
+        if (insertStr.charAt(i) === '\n') {
+          if (i === 0) {
+            lines.push('\n')
+          } else {
+            lines.push(insertStr.substring(start, i))
+            lines.push('\n')
+          }
+          start = i + 1
+        }
+      }
+
+      const tailStr = insertStr.substring(start)
+      if (tailStr) lines.push(tailStr)
+      lines.forEach(str => {
+        if (str === '\n') {
+          countMap.push(op.attributes && op.attributes.table || false)
+        }
+      })
+    }
+    return countMap
+  }, [])
 }
