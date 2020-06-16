af9d0e5e9530b345bc0f02caead9dfcb72379285
volser <sergey.voloshyn@gmail.com>
2020-06-16 21:23:04 +0300

    release

--- _develop/scripts/release.sh
+++ _develop/scripts/release.sh
@@ -14,7 +14,7 @@ rm -r dist
 mkdir .release
 mkdir .release/quill
 
-npm run build
+npm run build:webpack
 webpack --config _develop/webpack.config.js --env.minimize
 cp dist/quill.core.css dist/quill.bubble.css dist/quill.snow.css dist/quill.js dist/quill.core.js dist/quill.min.js dist/quill.min.js.map .release/quill/
 
