06bfbaf70b1dd30dd97eacedb99262cf739a6860
luowei <luowei@rensideMacBook-Pro.local>
2020-07-22 12:33:00 +0800

    Revert "[feature]Paste links over text"
    This reverts commit 79fe2b884b5d085154dd3180ce7596bed67bb2ac.

--- modules/clipboard.js
+++ modules/clipboard.js
@@ -22,7 +22,6 @@ import { FontStyle } from '../formats/font';
 import { SizeStyle } from '../formats/size';
 // clickup: utils
 import { _omit } from './clickup-table/utils';
-import Link from '../formats/link'
 
 const debug = logger('quill:clipboard');
 
@@ -157,21 +156,6 @@ class Clipboard extends Module {
     e.preventDefault();
     if (this.options.onCapturePaste.call(this, e)) return;
     const range = this.quill.getSelection(true);
-
-    // Paste links over text
-    if (range.length > 0) {
-      const copiedText = e.clipboardData.getData('text/plain');
-      const selectedLines = this.quill.getLines(range.index, range.length)
-      if (
-        isOneSingleLine(copiedText) &&
-        selectedLines.length === 1 &&
-        Link.sanitize(copiedText)
-      ) {
-        this.quill.format('link', copiedText)
-        this.quill.setSelection(range.index + range.length, 'api')
-        return;
-      }
-    }
 
     // Process pasting in table-cell-line before
     const [thisLeaf] = this.quill.getLine(range.index);
@@ -671,10 +655,6 @@ function matchText(node, delta) {
     }
   }
   return delta.insert(text);
-}
-
-function isOneSingleLine(text) {
-  return !(new RegExp('\\n', 'i').test(text))
 }
 
 export {
