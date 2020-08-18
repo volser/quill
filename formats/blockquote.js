d5e9a33f48451b910c6a16d2a63a241d621878a9
luowei <luowei@rensideMacBook-Pro.local>
2020-08-19 00:06:52 +0800

    [update]Blockquote: add shared container for blockquotes

--- formats/blockquote.js
+++ formats/blockquote.js
@@ -1,7 +1,21 @@
+import Quill from '../core/quill';
 import Block from '../blots/block';
+import Container from '../blots/container';
 
-class Blockquote extends Block {}
+export class BlockquoteContainer extends Container {}
+BlockquoteContainer.blotName = 'blockquote-container';
+BlockquoteContainer.className = 'ql-blockquote-container';
+BlockquoteContainer.tagName = 'DIV';
+
+class Blockquote extends Block {
+  static register() {
+    Quill.register(BlockquoteContainer);
+  }
+}
 Blockquote.blotName = 'blockquote';
 Blockquote.tagName = 'blockquote';
+
+BlockquoteContainer.allowedChildren = [Blockquote];
+Blockquote.requiredContainer = BlockquoteContainer;
 
 export default Blockquote;
