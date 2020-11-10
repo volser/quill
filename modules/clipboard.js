a6854990cb934f6a7dbd8c6b00a1df0018dd2562
loway <loway@MacBook-Pro.local>
2020-11-10 12:18:31 +0800

    [update]move paste in table cell logic to clickup app.

--- modules/clipboard.js
+++ modules/clipboard.js
@@ -64,11 +64,6 @@ const STYLE_ATTRIBUTORS = [
   return memo;
 }, {});
 
-const isExistTableInTheHTMLString = (htmlStr) => {
-  const normalizedHtmlStr = htmlStr.replace(/[\r\n]/g,"")
-  return new RegExp(/<(table).*\<\/\1\>/, 'i').test(normalizedHtmlStr)
-}
-
 class Clipboard extends Module {
   constructor(quill, options) {
     super(quill, options);
@@ -158,33 +153,6 @@ class Clipboard extends Module {
     if (this.options.onCapturePaste.call(this, e)) return;
     const range = this.quill.getSelection(true);
 
-    // Process pasting in table-cell-line before
-    const [thisLeaf] = this.quill.getLine(range.index);
-    if (thisLeaf && thisLeaf.constructor.name === 'TableCellLine') {
-      let html = e.clipboardData.getData('text/html');
-      const text = e.clipboardData.getData('text/plain');
-
-      // bugfix: Disable pasting tables inside the table it breaks the structure
-      if (isExistTableInTheHTMLString(html)) {
-        const normalizedHtmlStr = html.replace(/[\r\n]/g,"")
-        const tds = normalizedHtmlStr.match(/<(td).*?\<\/\1\>/g)
-        if (tds && tds.length > 1) {
-          return;
-        } else if (tds && tds.length === 1) {
-          html = html.replace(/<(colgroup).*?\<\/\1\>/g, '')
-        }
-      }
-
-      const files = Array.from(e.clipboardData.files || []);
-      if (!html && files.length > 0) {
-        this.quill.uploader.upload(range, files);
-      } else {
-        this.onTableCellPaste(range, { html, text });
-      }
-      return;
-    }
-
-    if (this.options.onCapturePaste.call(this, e)) return;
     if (range == null) return;
     const html = e.clipboardData.getData('text/html');
     const text = e.clipboardData.getData('text/plain');
@@ -244,80 +212,6 @@ class Clipboard extends Module {
       }
     });
     return [elementMatchers, textMatchers];
-  }
-
-  onTableCellPaste(range, { text, html }) {
-    const [line] = this.quill.getLine(range.index);
-    const lineFormats = line.formats();
-    const formats = this.quill.getFormat(range.index);
-    let pastedDelta = this.convert({ text, html }, formats);
-
-    pastedDelta = pastedDelta.reduce((newDelta, op) => {
-      if (op.insert && typeof op.insert === 'string') {
-        const lines = [];
-        let insertStr = op.insert;
-        let start = 0;
-        for (let i = 0; i < op.insert.length; i++) {
-          if (insertStr.charAt(i) === '\n') {
-            if (i === 0) {
-              lines.push('\n');
-            } else {
-              lines.push(insertStr.substring(start, i));
-              lines.push('\n');
-            }
-            start = i + 1;
-          }
-        }
-
-        const tailStr = insertStr.substring(start);
-        if (tailStr) lines.push(tailStr);
-
-        lines.forEach(text => {
-          if (text === '\n') {
-            if (op.attributes && op.attributes.list) {
-              // specify for paste list into table cell
-              const tableCellLineAttrs = lineFormats['table-cell-line'];
-              const listItemAttrs = extend(
-                {},
-                op.attributes,
-                tableCellLineAttrs,
-                {
-                  'table-cell-line': false,
-                  'list-container': false,
-                  list: {
-                    list: op.attributes.list.list,
-                    ...tableCellLineAttrs,
-                  },
-                  ...tableCellLineAttrs,
-                },
-              );
-              newDelta.insert('\n', extend({}, op.attributes, listItemAttrs));
-            } else {
-              // normal block line
-              newDelta.insert('\n', extend({}, op.attributes, lineFormats));
-            }
-          } else {
-            newDelta.insert(text, op.attributes);
-          }
-        });
-      } else {
-        newDelta.insert(op.insert, op.attributes);
-      }
-
-      return newDelta;
-    }, new Delta());
-
-    debug.log('onTableCellPaste', pastedDelta, { text, html });
-    const delta = new Delta()
-      .retain(range.index)
-      .delete(range.length)
-      .concat(pastedDelta);
-    this.quill.updateContents(delta, Quill.sources.USER);
-    this.quill.setSelection(
-      delta.length() - range.length,
-      Quill.sources.SILENT,
-    );
-    this.quill.scrollIntoView();
   }
 }
 Clipboard.DEFAULTS = {
